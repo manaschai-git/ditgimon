c7a5ab12ff66faba14526d06e33c4c5a17e5338a
noom <godfreedom191@gmail.com>
2026-06-16 23:02:40 +0700

    go2

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const http = require('http');
 const { OpenAI } = require('openai');
 const { createClient } = require('@supabase/supabase-js');
 
